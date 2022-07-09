9aa0db16953e24fa64c1448dab22714257b2ba2a
kassfir <kasparseglitisriga@gmail.com>
2022-07-09 12:45:14 +0300

    removed audio file concatenation because it takes too long and creates a horrible UX

--- communicate_with_cognigy.py
+++ communicate_with_cognigy.py
@@ -6,9 +6,6 @@ from gtts import gTTS
 import speech_recognition as sr
 import os
 from dotenv import load_dotenv
-from pydub import AudioSegment
-from tqdm import tqdm
-import pygame
 
 session_id = datetime.datetime.now().isoformat()
 user_id='user'
@@ -42,43 +39,6 @@ def send_message_to_cognigy(message_text, session_id, user_id):
     
     print (f'Cognigy response: {response_text_array}')
     return response_text_array
-
-#Taken from https://www.thepythoncode.com/article/concatenate-audio-files-in-python
-def concatenate_audio_pydub(audio_clip_paths, output_path, verbose=1):
-    """
-    Concatenates two or more audio files into one audio file using PyDub library
-    and save it to `output_path`. A lot of extensions are supported, more on PyDub's doc.
-    """
-    def get_file_extension(filename):
-        """A helper function to get a file's extension"""
-        return os.path.splitext(filename)[1].lstrip(".")
-
-    clips = []
-    # wrap the audio clip paths with tqdm if verbose
-    audio_clip_paths = tqdm(audio_clip_paths, "Reading audio file") if verbose else audio_clip_paths
-    for clip_path in audio_clip_paths:
-        # get extension of the audio file
-        extension = get_file_extension(clip_path)
-        print('####################################')
-        
-        print (clip_path)
-        print(extension)
-        # load the audio clip and append it to our list
-        clip = AudioSegment.from_file(clip_path, extension)
-        clips.append(clip)
-
-    final_clip = clips[0]
-    range_loop = tqdm(list(range(1, len(clips))), "Concatenating audio") if verbose else range(1, len(clips))
-    for i in range_loop:
-        # looping on all audio files and concatenating them together
-        # ofc order is important
-        final_clip = final_clip + clips[i]
-    # export the final clip
-    final_clip_extension = get_file_extension(output_path)
-    if verbose:
-        print(f"Exporting resulting audio file to {output_path}")
-    final_clip.export(output_path, format=final_clip_extension)
-
 
 def text_to_speech(text, filename='voice.mp3'):
     print(f'Saying: {text}')
